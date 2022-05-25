2c1ad5ae3af23e56bc376a0797b7c00ea22d1c74
Luzlly <102785056+Luzlly@users.noreply.github.com>
2022-05-25 14:47:53 +1000

    Upgrades Page Working

--- Assets/Scripts/Upgrades.cs
+++ Assets/Scripts/Upgrades.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Upgrades : MonoBehaviour
 {
     
     private VariableCheck varCheck;
+    public Text bottomText;
 
     public void Start()
     {
@@ -33,5 +35,25 @@ public class Upgrades : MonoBehaviour
         varCheck.upgAtk += 2;
         SceneManager.LoadScene("FightScene");
         Debug.Log("Loaded Scene: " + varCheck.sceneNum);
+    }
+
+    public void HoverAtk()
+    {
+        bottomText.text = "Increases Attack Power by 2";
+    }
+
+    public void HoverMH()
+    {
+        bottomText.text = "Increases Maximum HP by 5";
+    }
+
+    public void HoverHealing()
+    {
+        bottomText.text = "Increases Healing Potency by 3";
+    }
+
+    public void HoverExit()
+    {
+        bottomText.text = "";
     }
 }
